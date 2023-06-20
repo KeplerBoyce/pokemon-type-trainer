e1bcbb9f7fd1a8dd6eaa01e1cca39af85c3a49c9
Kepler Boyce <keplerboy@gmail.com>
2023-06-20 12:40:22 -0700

    change next config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-}
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: '/',
+};
 
-const isGithubActions = process.env.GITHUB_AACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace('/.*?\//', '')
-    assetPrefix = `/${repo}/`
-    basePath = `/${repo}`
-}
-
-module.exports = {
-    ...nextConfig,
-    assetPrefix,
-    basePath,
-}
+module.exports = nextConfig;
 
